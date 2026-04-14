e7caeea494e1cb177c6a0315313eab17a1881446
lavanya-k27 <lavanyakannan2710@gmail.com>
2026-04-14 20:23:43 +0530

    styles

--- accordion/src/App.jsx
+++ accordion/src/App.jsx
@@ -1,11 +1,12 @@
 import Accordion from "./Components/Accordion";
 import { items } from "./items";
+import "./App.css";
 
 const App = () => {
   return (
-    <>
+    <div className="container">
       <Accordion items={items} />
-    </>
+    </div>
   );
 };
 
