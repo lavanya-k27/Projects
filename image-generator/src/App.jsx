2dfe74cc0d50f0a275ba28c9648dedfcb4556637
lavanya-k27 <lavanyakannan2710@gmail.com>
2026-04-15 19:20:07 +0530

    done

--- image-generator/src/App.jsx
+++ image-generator/src/App.jsx
@@ -1,20 +1,42 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./Components/Header";
 import ImageGrid from "./Components/ImageGrid";
+import axios from "axios";
 import "./App.css";
 
 function App() {
   const [images, setImages] = useState([]);
 
-  const generateImage = () => {
+  useEffect(() => {
+    const fetchImages = async () => {
+      const response = await axios.get("http://localhost:5000/images");
+      setImages(response.data);
+    };
+
+    fetchImages();
+  }, []);
+
+  const generateImage = async () => {
     const imageUrl = "https://picsum.photos/200?" + Math.random();
-    setImages([...images, imageUrl]);
+
+    console.log(imageUrl);
+    const response = await axios.post("http://localhost:5000/images", {
+      image: imageUrl,
+    });
+    setImages([...images, response.data]);
+    console.log(response.data);
+  };
+
+  const handleDelete = async (id) => {
+    await axios.delete(`http://localhost:5000/images/${id}`);
+    const currImages = images.filter((image) => image.id !== id);
+    setImages(currImages);
   };
 
   return (
     <>
       <Header onClick={generateImage} />
-      <ImageGrid images={images} />
+      <ImageGrid images={images} handleDelete={handleDelete} />
     </>
   );
 }
