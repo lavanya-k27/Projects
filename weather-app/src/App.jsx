bdcc285a1d3394ac90f3b6ed045951f3d160a51f
lavanya-k27 <lavanyakannan2710@gmail.com>
2026-04-22 12:08:20 +0530

    weather

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import axios from "axios";
 import "./App.css";
 
+const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+
 function App() {
   const [city, setCity] = useState("");
   const [weather, setWeather] = useState(null);
@@ -11,10 +13,25 @@ function App() {
     setCity(e.target.value);
   };
 
-  const fetchWeather = () => {
+  const fetchWeather = async () => {
     if (!city) {
       setError("Please enter a city");
       return;
+    }
+    setError("");
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/find?q=${city}&appid=${API_KEY}&units=metric`,
+      );
+
+      console.log(response.data);
+      if (response.data.count > 0) {
+        setWeather(response.data.list[0]);
+      } else {
+        setError("City not found");
+      }
+    } catch (error) {
+      setError("An Error Occurred");
     }
   };
 
@@ -26,6 +43,14 @@ function App() {
       <div>
         <input placeholder="Enter city" onChange={handleChange} value={city} />
         <button onClick={fetchWeather}>search</button>
+        <div>{error && <p>{error}</p>}</div>
+        {weather && (
+          <div>
+            <h2>
+              {weather.name}, {weather.sys.country}
+            </h2>
+          </div>
+        )}
       </div>
     </div>
   );
